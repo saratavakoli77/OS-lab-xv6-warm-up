085307aec082876c420507dc70a259a449153cb4
Ghazal Minaei <ghminaei1037@gmail.com>
2019-10-05 23:18:02 +0330

    cpt fixed=))

--- cpt.c
+++ cpt.c
@@ -1,42 +1,31 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 char buf[512];
 
-// void
-// cpt(int fd_dest)
-// {
-//   int n;
+void
+cptFileToFile(int fd_src, int fd_dest)
+{
+  int n;
 
-//   while((n = read(fd, buf, sizeof(buf))) > 0) {
-//     if (write(1, buf, n) != n) {
-//       printf(1, "cat: write error\n");
-//       exit();
-//     }
-//   }
-//   if(n < 0){
-//     printf(1, "cat: read error\n");
-//     exit();
-//   }
-// }
+  while((n = read(fd_src, buf, sizeof(buf))) > 0) {
+    if (write(fd_dest, buf, n) != n) {
+      printf(1, "cpt: write error\n");
+      exit();
+    }
+  }
+}
 
 void
 cpt(int fd_src, int fd_dest)
 {
     int n;
 
-  while((n = read(fd_src, buf, sizeof(buf))) > 0) {
-      int adad = write(fd_dest, buf, n);
-      printf(1, "adad: %d\n", adad);
-      printf(1, "n: %d\n", n);
-    if (write(fd_dest, buf, n) != n) {
-      printf(1, "cpt: write error\n");
-      exit();
-    }
-  }
-  if(n < 0){
-    printf(1, "cpt: read error\n");
+  n = read(fd_src, buf, sizeof(buf));
+  if (write(fd_dest, buf, n) != n) {
+    printf(1, "cpt: write error\n");
     exit();
   }
 }
@@ -44,7 +33,7 @@ cpt(int fd_src, int fd_dest)
 int
 main(int argc, char *argv[])
 {
-  int fd;
+  int fd0, fd1;
 
   if(argc <= 1){
     printf(1, "cpt: missing file operand :/\n");
@@ -52,21 +41,28 @@ main(int argc, char *argv[])
   }
 
   if(argc == 2){
-    if((fd = open(argv[1], 0)) < 0){
+    if((fd0 = open(argv[1], O_CREATE|O_RDWR)) < 0){
         printf(1, "cpt: cannot open %s :/\n", argv[1]);
         exit();
     }
-    cpt(0, 2);
-    close(fd);
+    cpt(0, fd0);
+    close(fd0);
   }
 
-//   for(i = 1; i < argc; i++){
-//     if((fd = open(argv[i], 0)) < 0){
-//       printf(1, "cat: cannot open %s\n", argv[i]);
-//       exit();
-//     }
-//     cpt(fd);
-//     close(fd);
-//   }
+  if(argc == 3){
+
+    if((fd1 = open(argv[1], O_RDONLY)) < 0){
+        printf(1, "cpt: cannot open %s :/\n", argv[1]);
+        exit();
+    }
+    if((fd0 = open(argv[2], O_CREATE|O_RDWR)) < 0){
+        printf(1, "cpt: cannot open %s :/\n", argv[2]);
+        exit();
+    }
+    cptFileToFile(fd1, fd0);
+    close(fd0);
+    close(fd1);
+  }
+
   exit();
 }
